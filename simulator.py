a2597a5be1e9d89f94e4d92eb78b5e0073c5054e
enhuiz <niuzhe.nz@outlook.com>
2018-11-11 13:53:57 +0800

    update

---
+++ simulator.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+class Simulator(object):
+    """Simulator is an event generator.
+    It generates events in form of (tau, s),
+    where tau is the happening time (continuous) of that event
+    and s is the involved station id.
+    """
+
+    def __init__(self, data, delta_1, mu_r, t_r):
+        """
+        Args:
+            data: provided by xingbo
+            delta_1: the period length
+            mu_r: trike speed
+            t_r: loading time
+        """
+        self._data = data
+        self._delta_1 = delta_1
+        self._mu_r = mu_r
+        self._t_r = t_r
+
+    def simulate_rent_events(self, t):
+        """
+        Args:
+            t: period begining time
+        Returns:
+            [(tau, s)]: s is the station id, tau is the rent timestamp (continuous)
+        """
+        events = []
+        for i in range(100):
+            s = np.random.randint(0, 10)
+            tau = np.random.random() * 10
+            events.append((tau, s))
+        return events
+
+    def simulate_return_event(self, tau0, s0):
+        """
+        Args:
+            s0: the station where the bike is rent
+            tau0: the timestamp (continuous) when the bike is rent
+        Returns:
+            (tau, s): s is the station id, tau is the return timestamp (continous)
+        """
+        s = np.random.randint(0, 10)
+        while s == s0:
+            s = np.random.randint(0, 10)
+        tau = tau0 + np.random.random() * 10
+        return tau, s
+
+    def simulate_reposition_event(self, tau0, s0, s1):
+        """
+        Reposition Simulation, see equation (6) in the paper
+
+        Args:
+            tau0: the current time
+            s0: the station the trike are
+            s1: the station the trike will be
+        Returns:
+            (tau1, s1): tau1 the arrival time of the trike
+        """
+        d = self._data.query_distance(s0, s1)
+        epsilon = np.random.random()
+        tau1 = tau0 + d / self._mu_r + self._t_r + epsilon
+        return tau1, s1
+
