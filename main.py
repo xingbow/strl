ef80088a557a47df3d7bd6bcb1abf46ce142480b
enhuiz <niuzhe.nz@outlook.com>
2018-11-12 23:28:58 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from agent import DQNAgent
+from agent import DQNAgent, VanillaDQNAgent
 from env import Env
 from simulator import Simulator
 
@@ -6,12 +6,78 @@ import numpy as np
 
 num_regions = 10
 num_trikes = 5
-episode = [0, 600 * 1]
+episode = [0, 3600 * 1]
 capacity = 5
-num_epochs = 1000
+num_epochs = 100
 batch_size = 32
-delta = 60
+delta = 600
 rho = 2
+
+
+def run_baseline():
+    env = Env(simulator=Simulator(num_regions),
+              episode=episode,
+              num_regions=num_regions,
+              num_trikes=num_trikes,
+              capacity=capacity,
+              delta=delta,
+              rho=rho)
+
+    losses = []
+    num_epochs = 20
+    for epoch in range(num_epochs):
+        env.reset()
+        while not env.done:
+            env.step(action=[0, 0])
+        losses += [env.loss]
+        print('{}/{} loss {}, smooth loss {}'.format(
+            epoch, num_epochs,
+            losses[-1], np.mean(losses[-10:])))
+
+
+def run_vanilla_dqn():
+    env = Env(simulator=Simulator(num_regions),
+              episode=episode,
+              num_regions=num_regions,
+              num_trikes=num_trikes,
+              capacity=capacity,
+              delta=delta,
+              rho=rho)
+
+    action_size = num_regions * (2 * capacity + 1)
+    state_size = env._get_obs().shape[0]
+    agent = VanillaDQNAgent(state_size, action_size)
+
+    def encode_action(action):
+        r, a = action
+        ret = r + (a + capacity) * num_regions
+        return ret
+
+    def decode_action(action):
+        return [action % num_regions, action // num_regions - capacity]
+
+    assert all([encode_action(decode_action(a)) == a for a in range(1000)])
+
+    losses = []
+
+    for epoch in range(num_epochs):
+        state = env.reset()
+        while not env.done:
+            # if epoch % 10 == 0:
+            #     env.render()
+            action = agent.act(state)
+            next_state, raw_action, reward = env.step(decode_action(action))
+            action = encode_action(raw_action)
+
+            agent.remember(state, action, reward, next_state, env.done)
+            state = next_state
+
+        agent.replay(batch_size)
+        losses += [env.loss]
+
+        print('{}/{} loss {}, smooth loss {}'.format(
+            epoch, num_epochs,
+            losses[-1], np.mean(losses[-10:])))
 
 
 def run_dqn():
@@ -83,27 +149,7 @@ def run_dqn():
             losses[-1], np.mean(losses[-10:])))
 
 
-def run_baseline():
-    env = Env(simulator=Simulator(num_regions),
-              episode=episode,
-              num_regions=num_regions,
-              num_trikes=num_trikes,
-              capacity=capacity,
-              delta=delta,
-              rho=rho)
-
-    losses = []
-    num_epochs = 20
-    for epoch in range(num_epochs):
-        env.reset()
-        while not env.done:
-            env.step(action=[0, 0])
-        losses += [env.loss]
-        print('{}/{} loss {}, smooth loss {}'.format(
-            epoch, num_epochs,
-            losses[-1], np.mean(losses[-10:])))
-
-
 if __name__ == "__main__":
     run_baseline()
+    run_vanilla_dqn()
     run_dqn()
