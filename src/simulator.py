e8f7a6261d867f48bd1e69f7403bba3041b815e9
enhuiz <niuzhe.nz@outlook.com>
2018-11-14 00:30:59 +0800

    update interface

--- src/simulator.py
+++ src/simulator.py
@@ -1,16 +1,94 @@
 import numpy as np
+import pandas as pd
+import time
+import OIModel as om
+
+from collections import defaultdict
+
+
+def initialization():
+    # type in file loc
+    transitionDataLoc = '../data/dataOIModel.csv'  # transition file
+    stationStatusDataLoc = '../data/stationStatus.csv'  # station status file
+    weatherDataLoc = '../data/weather.csv'  # weather info file
+
+    # Initialize function, no need to modify
+    hisInputData, transitionMatrixDuration, transitionMatrixDestination, weatherMatrix = om.read_inData(
+        transitionDataLoc, stationStatusDataLoc, weatherDataLoc)
+
+    df = pd.read_csv('../data/station.csv')
+    df['limit'] = df['limit'].fillna(df['limit'].mean())
+    df = df.sort_values('id')
+    df = df.drop_duplicates('id')
+    ids = df['id']
+    id_to_ix = {v: i for i, v in enumerate(ids)}
+
+    points = df[['x', 'y']].values
+
+    def distance_between(i, j):
+        a = points[i]
+        b = points[j]
+        a, b = map(np.array, [a, b])
+        return np.sum((a - b)**2)**0.5
+
+    dist = defaultdict(dict)
+    for id1 in ids:
+        for id2 in ids:
+            i = id_to_ix[id1]
+            j = id_to_ix[id2]
+            dist[i][j] = distance_between(i, j)
+            dist[j][i] = dist[i][j]
+
+    def distance(i, j):
+        return dist[i][j]
+
+    def nearest(i):
+        return max(dist[i].items(), lambda kv: kv[1])[0]
+
+    # Inputs time stamp : e.g. 1373964540
+    # Inputs station ID : e.g.212
+    # return: expected departure(rent) number of this station in a period(hour)
+
+    def demands(t, i):
+        return om.get_expectDepartureNumber(
+            t, ids[i],
+            hisInputData,
+            weatherMatrix)
+
+    # Inputs time stamp : e.g. 1373964540
+    # Inputs station ID : e.g.212
+    # return: expected destination and departure time
+    def destination(t, i):
+        return om.get_predictedDestination(
+            t, ids[i],
+            transitionMatrixDuration,
+            transitionMatrixDestination)[0]
+
+    # Inputs time stamp : e.g. 1373964540
+    # Inputs start station ID : e.g.212
+    # Inputs end station ID : e.g.404
+    # return: expected time used
+    def duration(t, i, j):
+        return om.get_predictedDuration(
+            t, ids[i], ids[j],
+            transitionMatrixDuration,
+            transitionMatrixDestination)
+
+    def query_limits():
+        return df['limit'].values
+
+    return demands, destination, duration, distance, nearest, query_limits
+
+
+demands, destination, duration, \
+    distance, nearest, query_limits = initialization()
 
 
 class Simulator(object):
-    def __init__(self, num_regions, real_mode=False):
-        """
-        Args:
-            real_mode: real world case or not, if real, simulate all, otherwise, query all
-        """
-        self._num_regions = num_regions
-        self._distance = 1000 * \
-            np.abs(np.random.normal(
-                size=[self._num_regions, self._num_regions]))
+    def __init__(self, mu, tr, er):
+        self.mu = mu
+        self.tr = tr
+        self.er = er
 
     def get_rent_events(self, episode):
         """
@@ -19,30 +97,25 @@ class Simulator(object):
         Returns:
             [(t, r)]: t is the rent timestamp (continuous), r is the region id
         """
-        events = []
-        for i in range(1000):
-            r = np.random.randint(0, self._num_regions)
-            t = np.random.random() * 1000
-            events.append((t, r))
-        return events
+        return demands(episode, None)
 
-    def get_likely_region(self, t, r):
+    def get_likely_destination(self, t, r):
         """
         Needed when generate return event
         """
-        return np.random.randint(0, self._num_regions)
+        return destination(t, r)
 
-    def get_bike_arrival_time(self, t, ra, rb):
+    def get_bike_arrival_time(self, t, r0, r1):
         """
         Needed when generate return event
         """
-        return t + self.get_distance(ra, rb) / 10
+        return duration(t, r0, r1)
 
-    def get_trike_arrival_time(self, t, ra, rb):
+    def get_trike_arrival_time(self, t, r0, r1):
         """
         Needed when generate reposition
         """
-        return t + self.get_distance(ra, rb) / 50
+        return t + self.get_distance(r0, r1) / self.mu + self.tr + np.random.normal(0, self.er)
 
     def get_nearest_region(self, r):
         """
@@ -52,10 +125,13 @@ class Simulator(object):
         Returns:
             nearest region
         """
-        return np.argmax(self._distance[r])
+        return nearest(r)
 
-    def get_distance(self, ra, rb):
+    def get_distance(self, r0, r1):
         """
         Needed when estimate time for trike reposition
         """
-        return self._distance[ra, rb]
+        return distance(r0, r1)
+
+    def get_limits(self):
+        return query_limits()
