0990cf9dfd79f400ab928ccf2442185711259836
Zhenhua Xu <tonyxuhit@gmail.com>
2018-11-14 19:29:27 +0800

    Update testOIModel.py

--- testOIModel.py
+++ testOIModel.py
@@ -15,17 +15,16 @@ weatherDataLoc = 'C:/Users/Tony Xu/OneDrive/KDD 2/weather.csv' # weather info fi
 # Initialize function, no need to modify
 hisInputData, transitionMatrixDuration, transitionMatrixDetination, weatherMatrix ,entireSituation,weatherMatrixAll\
     = om.read_inData(transitionDataLoc,weatherDataLoc)
+timeStampBound = 1361667600
+overAllTest, pred_y, dayNumBound = om.predicted_overallSituation(dayNumBound, entireSituation, weatherMatrixAll)
 
 def test():
     # Inputs time stamp : e.g. 1373964540
     # Inputs station ID : e.g.212
     # return: expected departure(rent) number of this station in a period(hour)
     timeStamp = 1393465860
-    timeStampBound = timeStamp
     startStationID = 11
     dayNumBound = (timeStampBound-startTime)//(24*3600)
-    print (dayNumBound)
-    overAllTest, pred_y, dayNumBound = om.predicted_overallSituation(dayNumBound, entireSituation, weatherMatrixAll)
     expectedDepartureTime = om.get_expectDepartureNumber(timeStamp,startStationID,hisInputData,weatherMatrix,overAllTest,pred_y,dayNumBound)
 
     # Inputs time stamp : e.g. 1373964540
